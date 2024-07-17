3d04148bb15150be700e08d45a6eecd1d0eb3d08
HasnainAR <101927980+HasnainAR@users.noreply.github.com>
2024-07-18 03:25:17 +0500

    Client setup and routing

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <div
+        className="container"
+        style={{
+          maxWidth: "140rem",
+          padding: "4.2rem 2.4rem",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="logo-brand">
+          <a href="/">Hasnain</a>
+        </div>
+        <nav>
+          <ul style={{
+          display: "flex",
+          gap: "3.2rem",
+          
+        }}>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">Service</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">login</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
