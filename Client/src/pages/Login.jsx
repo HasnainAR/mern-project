5e61d109d67047b68edc8875b50e529d95592841
HasnainAR <101927980+HasnainAR@users.noreply.github.com>
2024-07-19 23:11:14 +0500

    Login

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //avoid refresh
+  };
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div
+              className="container grid grid-two-cols"
+              style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr",
+                gap: "00px", // Adjust gap as needed
+              }}
+            >
+              <div className="registration-image reg-img">
+                <img
+                  src="/images/login.png"
+                  alt=""
+                  width="400"
+                  height="500"
+                />
+              </div>
+
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Login form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="email"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
+export default Login;
